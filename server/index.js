278a5bbe6264e5fa9127e166d9f2af54b5e40547
Jeff Whittington <jeff@codecology.com>
2019-12-10 12:38:47 -0800

    Changes for proxy servers

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ app.use(express.static(__dirname + '/../public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 //  get all reservations (for testing)
 app.get('/reservation/all', function (req, res) {
   Reservation.getAll()
