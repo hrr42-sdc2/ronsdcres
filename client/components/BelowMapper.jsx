bafab6788c849895ac0cde25b0188e5cea66cdec
Jeff Whittington <jeff@codecology.com>
2019-12-16 07:59:13 -0800

    Styling changes

--- client/components/BelowMapper.jsx
+++ client/components/BelowMapper.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 
 var BelowMapper = () => (
   <div className="below-mapper">
-    <div class="streetBox">
+    <div className="streetBox">
       <img style={{marginTop: '5px', marginBottom: '-8px'}} src="assets/ic_location.png"></img>
       &nbsp;1313 Mockingbird Lane, San Francisco, CA
     </div>
