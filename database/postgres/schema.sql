6f04f63469888311043becc9447ac7e485e888c5
partorican <ibringwater@gmail.com>
2019-12-31 01:40:35 -0700

    created schema.sql

--- database/postgres/schema.sql
+++ database/postgres/schema.sql
@@ -21,3 +21,5 @@ CREATE TABLE reservationSchema (
   created_at DATETIME,
   update_at DATETIME
 );
+
+/* STILL NEED TO CREATE THE HAS MANY, BELONGS TO PART https://stackoverflow.com/questions/9789736/how-to-implement-a-many-to-many-relationship-in-postgresql */
